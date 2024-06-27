af52458aecacb3d74d5d305eb137b25be14326b2
9ightshade <aniekemeusanga@gmail.com>
2024-06-27 22:01:59 +0100

    update

---
+++ 0x1A-application_server/web01.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#+ # Script to start the gunicorn server serving task 0 of web_flask
+#+ # Environment variables here
+HBNB_ENV=test
+HBNB_MYSQL_USER=hbnb_test
+HBNB_MYSQL_PWD=hbnb_test_pwd
+HBNB_MYSQL_HOST=localhost
+HBNB_MYSQL_DB=hbnb_test_db
+HBNB_TYPE_STORAGE=db
+
+gunicorn --bind 0.0.0.0:5003 web_dynamic.2-hbnb:app
